07f550d10437ee018cfcd248812a23ace46b4439
Ali Rehmatullah <rehmatullah.ali@gmail.com>
2018-07-08 02:12:11 -0400

    Set up linter and copy webpack plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 const HtmlWebPackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
   module: {
@@ -49,6 +49,10 @@ module.exports = {
     new MiniCssExtractPlugin({
       filename: "[name].css",
       chunkFilename: "[id].css"
-    })
+    }),
+    new CopyWebpackPlugin([{
+      from: './src/images',
+      to: 'images/'
+    }])
   ]
 }
